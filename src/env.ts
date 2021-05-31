16ab7d364e8994bd039658ec56e5548b57a555fc
al-chaudhari <chaudharia041@gmail.com>
2021-05-31 16:27:40 +0530

    FEAT: added env

--- src/env.ts
+++
@@ -1,5 +0,0 @@
-export const apiKey = '8dac501823c6536';
-export const apiSecret = '1eb75bebc28a815';
-export const host = 'http://localhost:8080';
-export const popupId = 'erpnextpopup';
-export const popupelementId= 'erpnext__popup'
